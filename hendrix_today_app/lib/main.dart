9167df2eaccbf07ff2a13aa8393c2670449301ae
mason-pAoLo <masonmillionaire725@gmail.com>
2022-10-27 13:37:59 -0500

    Create GUI Base
    Bottom Nav Bar to switch between pages

--- hendrix_today_app/lib/main.dart
+++ hendrix_today_app/lib/main.dart
@@ -1,38 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: ScreenContainer()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ScreenContainer extends StatefulWidget {
+  const ScreenContainer({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<ScreenContainer> createState() => _ScreenContainerState();
+}
+
+class _ScreenContainerState extends State<ScreenContainer> {
+  int selectedIndex = 0;
+  final List<Widget> pages = [MyHomeScreen(), TBDScreen()];
+  final List<String> titles = ["Homes", "TBD"];
+  final Color webOrange = Color.fromARGB(255, 202, 81, 39);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 202, 81, 39),
+            title: Text(titles[selectedIndex])),
+        body: Center(child: pages[selectedIndex]),
+        bottomNavigationBar:
+            BottomNavigationBar(items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Color.fromARGB(255, 202, 81, 39),
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.tab,
+                color: webOrange,
+              ),
+              label: "TBD")
+        ], currentIndex: selectedIndex, onTap: onItemTapped));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyHomeScreen extends StatefulWidget {
+  const MyHomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomeScreen> createState() => _MyHomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
-
-
+class _MyHomeScreenState extends State<MyHomeScreen> {
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -41,22 +69,15 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),body: ListView(
+    return Center(
+      child: ListView(
         children: <Widget>[
           ListTile(
             title: Text('Theme'),
             onTap: () {
               AlertDialog alert = AlertDialog(
                 title: Text('Event Here'),
-                content: Column(
-                  children: [
-                    Text("Event description here")
-                  ]),
+                content: Column(children: [Text("Event description here")]),
               );
               showDialog(
                 context: context,
@@ -68,6 +89,22 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-      );
+    );
+  }
+}
+
+class TBDScreen extends StatefulWidget {
+  const TBDScreen({super.key});
+
+  @override
+  State<TBDScreen> createState() => _TBDScreenState();
+}
+
+class _TBDScreenState extends State<TBDScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello World'),
+    );
   }
 }
