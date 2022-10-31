4fca19ecb51a20480dbfcd02f199a2aa21c685ee
mason-pAoLo <masonmillionaire725@gmail.com>
2022-10-31 14:32:09 -0500

    Slightly elevated cards so they appear more prominent

--- hendrix_today_app/lib/home_screen.dart
+++ hendrix_today_app/lib/home_screen.dart
@@ -14,45 +14,48 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
       child: ListView(
         children: <Widget>[
           Card(
+              elevation: 6.0,
               child: ListTile(
-            title: Text('Farewell to Dean Wiltgen'),
-            subtitle: Text('October 31st, 2-4pm'),
-            onTap: () {
-              AlertDialog alert = AlertDialog(
                 title: Text('Farewell to Dean Wiltgen'),
-                content: Column(children: [
-                  Text(
-                      "You are invited to drop by the SLTC Student Services Suite today from 2-4 p.m. to say farewell to Dean Wiltgen. There will be refreshments while they last!")
-                ]),
-              );
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return alert;
+                subtitle: Text('October 31st, 2-4pm'),
+                onTap: () {
+                  AlertDialog alert = AlertDialog(
+                    title: Text('Farewell to Dean Wiltgen'),
+                    content: Column(children: [
+                      Text(
+                          "You are invited to drop by the SLTC Student Services Suite today from 2-4 p.m. to say farewell to Dean Wiltgen. There will be refreshments while they last!")
+                    ]),
+                  );
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return alert;
+                    },
+                  );
                 },
-              );
-            },
-          )),
+              )),
           Card(
-              child: ListTile(
-            title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-            subtitle: Text('October 31st, 4-5pm'),
-            onTap: () {
-              AlertDialog alert = AlertDialog(
-                title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-                content: Column(children: [
-                  Text(
-                      "Come out to the SLTC Kitchen today from 4-5 p.m. and help kick off National French Week at our Crêpe extravaganza! All are welcome! Be sure to check out all of the festivities planned this week.")
-                ]),
-              );
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return alert;
-                },
-              );
-            },
-          ))
+            elevation: 6.0,
+            child: ListTile(
+              title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
+              subtitle: Text('October 31st, 4-5pm'),
+              onTap: () {
+                AlertDialog alert = AlertDialog(
+                  title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
+                  content: Column(children: [
+                    Text(
+                        "Come out to the SLTC Kitchen today from 4-5 p.m. and help kick off National French Week at our Crêpe extravaganza! All are welcome! Be sure to check out all of the festivities planned this week.")
+                  ]),
+                );
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return alert;
+                  },
+                );
+              },
+            ),
+          )
         ],
       ),
     );
