b3b262b9f943c42a9b48287f6e24caec93ef1a4e
Sarah Wright <sew104312@gmail.com>
2022-11-07 12:35:01 -0600

    Stores all necessary parameters as strings and has a date split function
    The function takes in the full string for the date and splits it into a list that just has the numbers in it.

--- hendrix_today_app/lib/event_items.dart
+++ hendrix_today_app/lib/event_items.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 String title = '';
 String description = '';
 String event_date = '';
@@ -9,16 +11,19 @@ String final_post = '';
 class event_item{
   final String title;
   final String description;
-  final String event_date;
+  final String event_date_begin;
   final String begin_post;
   final String final_post;
+  final String event_date_end;
+  final String event_time;
   
   event_item({required this.title, required this.description,
-   required this.event_date, required this.begin_post, required this.final_post});
+   required this.event_date_begin, required this.event_date_end, required this.event_time,
+    required this.begin_post, required this.final_post});
   
-  date_split(){
-    begin_post.split('/');
-    final_post.split('/');
-
+  List date_split(String parts){
+    int idx = begin_post.indexOf("/");
+    List split = [parts.substring(0,idx).trim(), parts.substring(idx+1).trim()];
+    return split;
   }
 }
