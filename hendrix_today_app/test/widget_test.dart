97f40685653894d3f03ef9233a41cbe43aa93b43
Ethan Thomas <thomasem@hendrix.edu>
2022-11-14 12:43:35 -0600

    search screen primary test

--- hendrix_today_app/test/widget_test.dart
+++ hendrix_today_app/test/widget_test.dart
@@ -1,7 +1,9 @@
+import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hendrix_today_app/home_screen.dart';
 import 'package:hendrix_today_app/calendar_screen.dart';
+import 'package:hendrix_today_app/search_screen.dart';
 
 void main() {
   testWidgets('Home Screen is populated', (tester) async {
@@ -17,5 +19,12 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.byType(EventCalendar), findsOneWidget);
+  });
+
+  testWidgets('Search Screen exists and has searchBar', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: SearchScreen()));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(AnimSearchBar), findsOneWidget);
   });
 }
