d91634385171791c262605dc394cfa0971758252
mason-pAoLo <masonmillionaire725@gmail.com>
2022-11-29 22:43:49 -0600

    Added Menu Viewer

--- hendrix_today_app/lib/home_screen.dart
+++ hendrix_today_app/lib/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 var daily_events_list;
 var daily_announcements_list;
@@ -15,26 +18,52 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children:[SizedBox(
+        child: Column(
+      children: [
+        SizedBox(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height - 200,
           child: ListView(
             key: daily_events_list,
             children: <Widget>[
-            Image.asset('assets/webOrange_banner.png', key: const Key("Banner")),
-            Card(
+              Image.asset('assets/webOrange_banner.png',
+                  key: const Key("Banner")),
+              Card(
+                  elevation: 6.0,
+                  child: ListTile(
+                    title: Text('Farewell to Dean Wiltgen'),
+                    subtitle: Text('October 31st, 2-4pm'),
+                    onTap: () {
+                      AlertDialog alert = AlertDialog(
+                        title: Text('Farewell to Dean Wiltgen'),
+                        insetPadding:
+                            EdgeInsets.symmetric(vertical: 200, horizontal: 50),
+                        content: Column(children: [
+                          Text(
+                              "You are invited to drop by the SLTC Student Services Suite today from 2-4 p.m. to say farewell to Dean Wiltgen. There will be refreshments while they last!")
+                        ]),
+                      );
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return alert;
+                        },
+                      );
+                    },
+                  )),
+              Card(
                 elevation: 6.0,
                 child: ListTile(
-                  title: Text('Farewell to Dean Wiltgen'),
-                  subtitle: Text('October 31st, 2-4pm'),
+                  title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
+                  subtitle: Text('October 31st, 4-5pm'),
                   onTap: () {
                     AlertDialog alert = AlertDialog(
-                      title: Text('Farewell to Dean Wiltgen'),
-                      insetPadding: EdgeInsets.symmetric(vertical: 200, horizontal: 50),
+                      title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
+                      insetPadding:
+                          EdgeInsets.symmetric(vertical: 200, horizontal: 50),
                       content: Column(children: [
                         Text(
-                            "You are invited to drop by the SLTC Student Services Suite today from 2-4 p.m. to say farewell to Dean Wiltgen. There will be refreshments while they last!")
+                            "Come out to the SLTC Kitchen today from 4-5 p.m. and help kick off National French Week at our Crêpe extravaganza! All are welcome! Be sure to check out all of the festivities planned this week.")
                       ]),
                     );
                     showDialog(
@@ -44,35 +73,20 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
                       },
                     );
                   },
-                )),
-            Card(
-              elevation: 6.0,
-              child: ListTile(
-                title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-                subtitle: Text('October 31st, 4-5pm'),
-                onTap: () {
-                  AlertDialog alert = AlertDialog(
-                    title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-                    insetPadding: EdgeInsets.symmetric(vertical: 200, horizontal: 50),
-                    content: Column(children: [
-                      Text(
-                          "Come out to the SLTC Kitchen today from 4-5 p.m. and help kick off National French Week at our Crêpe extravaganza! All are welcome! Be sure to check out all of the festivities planned this week.")
-                    ]),
-                  );
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return alert;
-                    },
-                  );
-                },
-              ),
-            )
-          ],
-      ),
+                ),
+              )
+            ],
+          ),
         ),
-    ],
-    )
-    );
+        Link(
+            target: LinkTarget.blank,
+            uri: Uri.parse(
+                'https://www.hendrix.edu/diningservices/default.aspx?id=1005'),
+            builder: (context, followLink) => InkWell(
+                  onTap: followLink,
+                  child: const Text("Today's Menu"),
+                ))
+      ],
+    ));
   }
 }
