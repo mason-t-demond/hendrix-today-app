0e4ea551c07be50ef3d0b28e3a14a3af1ee4ff3b
Sarah Wright <sew104312@gmail.com>
2022-11-30 11:05:54 -0600

    Added the same hard coded events to gui A

--- hendrix_today_app/lib/home_screen.dart
+++ hendrix_today_app/lib/home_screen.dart
@@ -22,19 +22,20 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
           child: ListView(
             key: daily_events_list,
             children: <Widget>[
-            Image.asset('assets/webOrange_banner.png', key: const Key("Banner")),
-            Card(
-                elevation: 6.0,
-                child: ListTile(
-                  title: Text('Farewell to Dean Wiltgen'),
-                  subtitle: Text('October 31st, 2-4pm'),
-                  onTap: () {
+              Image.asset('assets/webOrange_banner.png',
+                  key: const Key("Banner")),
+              Card(
+          child: ListTile(
+            title: Text("TEC Talk Less, Smile More Policy Battles"), 
+            trailing: Text("12/02/2022"),
+            subtitle: Text("Event"),
+            onTap: () {
                     AlertDialog alert = AlertDialog(
-                      title: Text('Farewell to Dean Wiltgen'),
-                      insetPadding: EdgeInsets.symmetric(vertical: 200, horizontal: 50),
+                      title: Text("TEC Talk Less, Smile More Policy Battles"),
+                      insetPadding:
+                          EdgeInsets.symmetric(vertical: 200, horizontal: 50),
                       content: Column(children: [
-                        Text(
-                            "You are invited to drop by the SLTC Student Services Suite today from 2-4 p.m. to say farewell to Dean Wiltgen. There will be refreshments while they last!")
+                        Text("Join the Talk Less, Smile More TEC class for performances of their Policy Battles. In these battles, performers will artistically debate about immigration and environmental policies. Two performances: today, and Friday, Dec. 2, from 12:10 -1 p.m. in Hundley-Shell Theater.")
                       ]),
                     );
                     showDialog(
@@ -44,31 +45,51 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
                       },
                     );
                   },
-                )),
+            )),
             Card(
-              elevation: 6.0,
-              child: ListTile(
-                title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-                subtitle: Text('October 31st, 4-5pm'),
-                onTap: () {
-                  AlertDialog alert = AlertDialog(
-                    title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-                    insetPadding: EdgeInsets.symmetric(vertical: 200, horizontal: 50),
-                    content: Column(children: [
-                      Text(
-                          "Come out to the SLTC Kitchen today from 4-5 p.m. and help kick off National French Week at our Crêpe extravaganza! All are welcome! Be sure to check out all of the festivities planned this week.")
-                    ]),
-                  );
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return alert;
-                    },
-                  );
-                },
-              ),
-            )
-          ],
+          child: ListTile(
+            title: Text("KHDX Rave Wristbands!"), 
+            subtitle: Text("Announcement"),
+            onTap: () {
+                    AlertDialog alert = AlertDialog(
+                      title: Text("KHDX Rave Wristbands!"),
+                      insetPadding:
+                          EdgeInsets.symmetric(vertical: 200, horizontal: 50),
+                      content: Column(children: [
+                        Text("KHDX is handing out wristbands for the Rave this week! Wednesday and Friday from 11-2 and Thursday from 4-7 p.m. Guests will be registered during these times and must bring their ID for entry at the door. Rave is Dec. 2, 9 p.m.–1 a.m. in Worsham.")
+                      ]),
+                    );
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return alert;
+                      },
+                    );
+                  },
+            )),
+            Card(
+          child: ListTile(
+            title: Text("DANC 250 Dance Making Fundamentals Final Performances"), 
+            trailing: Text("12/06/2022"),
+            subtitle: Text("Event"),
+            onTap: () {
+                    AlertDialog alert = AlertDialog(
+                      title: Text("DANC 250 Dance Making Fundamentals Final Performances"),
+                      insetPadding:
+                          EdgeInsets.symmetric(vertical: 200, horizontal: 50),
+                      content: Column(children: [
+                        Text("On Dec. 6 at 10:30 a.m. in Trieschmann Dance Studio, students will perform final choreographic projects and hold a Q&A after all 14 solo pieces have been performed. ")
+                      ]),
+                    );
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return alert;
+                      },
+                    );
+                  },
+            )),     
+            ],
       ),
         ),
     ],
