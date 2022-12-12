8d1f7aed85ad6092d0892b5890b10a7dac0473af
Ethan Thomas <thomasem@hendrix.edu>
2022-12-11 21:46:02 -0600

    FB data in Home Screen, no UI currently

--- hendrix_today_app/lib/home_screen.dart
+++ hendrix_today_app/lib/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'firebase.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'reading_excel.dart' as rb;
+import 'firebase.dart' as fb;
 
 var daily_events_list;
 var daily_announcements_list;
@@ -13,70 +17,36 @@ class MyHomeScreen extends StatefulWidget {
 }
 
 class _MyHomeScreenState extends State<MyHomeScreen> {
+  final Stream<QuerySnapshot> _usersStream =
+      fb.db.collection('eventsListed').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height - 200,
-          child: ListView(
-            key: daily_events_list,
-            children: <Widget>[
-              Image.asset('assets/webOrange_banner.png',
-                  key: const Key("Banner")),
-              Card(
-                  elevation: 6.0,
-                  child: ListTile(
-                    title: Text('Farewell to Dean Wiltgen'),
-                    subtitle: Text('October 31st, 2-4pm'),
-                    onTap: () {
-                      AlertDialog alert = AlertDialog(
-                        title: Text('Farewell to Dean Wiltgen'),
-                        insetPadding:
-                            EdgeInsets.symmetric(vertical: 200, horizontal: 50),
-                        content: Column(children: [
-                          Text(
-                              "You are invited to drop by the SLTC Student Services Suite today from 2-4 p.m. to say farewell to Dean Wiltgen. There will be refreshments while they last!")
-                        ]),
-                      );
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return alert;
-                        },
-                      );
-                    },
-                  )),
-              Card(
-                elevation: 6.0,
-                child: ListTile(
-                  title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-                  subtitle: Text('October 31st, 4-5pm'),
-                  onTap: () {
-                    AlertDialog alert = AlertDialog(
-                      title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-                      insetPadding:
-                          EdgeInsets.symmetric(vertical: 200, horizontal: 50),
-                      content: Column(children: [
-                        Text(
-                            "Come out to the SLTC Kitchen today from 4-5 p.m. and help kick off National French Week at our Crêpe extravaganza! All are welcome! Be sure to check out all of the festivities planned this week.")
-                      ]),
-                    );
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return alert;
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    ));
+    return StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['title']),
+                  subtitle: Text(data['date']),
+                );
+              })
+              .toList()
+              .cast(),
+        );
+      },
+    );
   }
 }
