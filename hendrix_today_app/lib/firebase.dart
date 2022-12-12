97b3c881ba0f4b8d72c97cb3613bc8ea1881db48
Sarah Wright <sew104312@gmail.com>
2022-12-11 20:32:35 -0600

    changed name of collection

--- hendrix_today_app/lib/firebase.dart
+++ hendrix_today_app/lib/firebase.dart
@@ -44,7 +44,7 @@ class Event {
 }
 
 Future getRef()async{
-final ref = db.collection("events").doc("event1").withConverter(
+final ref = db.collection("eventsListed").doc("event1").withConverter(
     fromFirestore: Event.fromFirestore,
     toFirestore: (Event event, _) => event.toFirestore(),
     );
