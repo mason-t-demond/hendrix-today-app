559987051e8cafbfcdc4093e98d05dc8f1a4cb71
Ethan Thomas <thomasem@hendrix.edu>
2022-12-11 23:50:57 -0600

    cleaned up home screen, no more commented code

--- hendrix_today_app/lib/home_screen.dart
+++ hendrix_today_app/lib/home_screen.dart
@@ -16,6 +16,8 @@ class MyHomeScreen extends StatefulWidget {
   State<MyHomeScreen> createState() => _MyHomeScreenState();
 }
 
+//got the code for below from: https://firebase.google.com/docs/firestore/query-data/listen#dart_1
+
 class _MyHomeScreenState extends State<MyHomeScreen> {
   final Stream<QuerySnapshot> _usersStream =
       fb.db.collection('eventsListed').snapshots();
@@ -32,19 +34,14 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Text("Loading");
         }
-        //return Center(
-        // return Column(
-        //   children: [
+
         return SizedBox(
           width: MediaQuery.of(context).size.width,
-          //height: MediaQuery.of(context).size.height - 200,
           child: ListView(
             key: const Key('daily_events_list'),
             children: <Widget>[
               Image.asset('assets/webOrange_banner.png',
                   key: const Key("Banner")),
-              //ListView(
-              //children:
               ListBody(
                   children: snapshot.data!.docs
                       .map((DocumentSnapshot document) {
@@ -73,77 +70,10 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
                       })
                       .toList()
                       .cast()),
-              //),
             ],
           ),
         );
-        //],
-        //),
-        //);
       },
     );
-    // return Center(
-    //     child: Column(
-    //   children: [
-    //     SizedBox(
-    //       width: MediaQuery.of(context).size.width,
-    //       height: MediaQuery.of(context).size.height - 200,
-    //       child: ListView(
-    //         key: daily_events_list,
-    //         children: <Widget>[
-    //           Image.asset('assets/webOrange_banner.png',
-    //               key: const Key("Banner")),
-    //           Card(
-    //               elevation: 6.0,
-    //               child: ListTile(
-    //                 title: Text('Farewell to Dean Wiltgen'),
-    //                 subtitle: Text('October 31st, 2-4pm'),
-    //                 onTap: () {
-    //                   AlertDialog alert = AlertDialog(
-    //                     title: Text('Farewell to Dean Wiltgen'),
-    //                     insetPadding:
-    //                         EdgeInsets.symmetric(vertical: 200, horizontal: 50),
-    //                     content: Column(children: [
-    //                       Text(
-    //                           "You are invited to drop by the SLTC Student Services Suite today from 2-4 p.m. to say farewell to Dean Wiltgen. There will be refreshments while they last!")
-    //                     ]),
-    //                   );
-    //                   showDialog(
-    //                     context: context,
-    //                     builder: (BuildContext context) {
-    //                       return alert;
-    //                     },
-    //                   );
-    //                 },
-    //               )),
-    //           Card(
-    //             elevation: 6.0,
-    //             child: ListTile(
-    //               title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-    //               subtitle: Text('October 31st, 4-5pm'),
-    //               onTap: () {
-    //                 AlertDialog alert = AlertDialog(
-    //                   title: Text('NATIONAL FRENCH WEEK - Crêpe Extravaganza!'),
-    //                   insetPadding:
-    //                       EdgeInsets.symmetric(vertical: 200, horizontal: 50),
-    //                   content: Column(children: [
-    //                     Text(
-    //                         "Come out to the SLTC Kitchen today from 4-5 p.m. and help kick off National French Week at our Crêpe extravaganza! All are welcome! Be sure to check out all of the festivities planned this week.")
-    //                   ]),
-    //                 );
-    //                 showDialog(
-    //                   context: context,
-    //                   builder: (BuildContext context) {
-    //                     return alert;
-    //                   },
-    //                 );
-    //               },
-    //             ),
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   ],
-    // ));
   }
 }
